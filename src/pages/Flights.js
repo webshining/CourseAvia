c3cbde931e11e2aae6806370097f94160c092fcc
WebShining <79418317+webshining@users.noreply.github.com>
2022-06-13 12:01:46 +0300

    Update App

--- src/pages/Flights.js
+++ src/pages/Flights.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import '../styles/flights.scss'
 
 const Flights = () => {
     return (
         <div className="flights">
-            
+            <table>
+                <thead>
+                    <tr>
+                        <th>Номер рейсу</th>
+                        <th>Віжправлення</th>
+                        <th>Прибуття</th>
+                        <th>Час В.</th>
+                        <th>Час П.</th>
+                        <th>Тривалість</th>
+                    </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                </tbody>
+            </table>
         </div>
     );
 };
